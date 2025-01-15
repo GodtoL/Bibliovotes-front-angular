b2801ac6c10d359302bfdde3ff6240e08c167209
GodtoL <eliasvera793@gmail.com>
2025-01-15 19:48:35 -0300

    Se agrega el header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BookListComponent } from './components/book-list/book-list.component';
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BookListComponent],
+  imports: [RouterOutlet, BookListComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
