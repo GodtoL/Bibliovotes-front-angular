11de2df254fd9a8194c2b6089f63b198987d1dec
GodtoL <eliasvera793@gmail.com>
2025-01-15 21:17:44 -0300

    Se agrega tag service

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
+import { TagService } from '../../services/tag.service';
 
 @Component({
   selector: 'app-book-list',
@@ -7,18 +8,28 @@ import { ApiService } from '../../services/api.service';
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css'
 })
+
 export class BookListComponent implements OnInit {
 
   books : any = [];
-  constructor(private apiService : ApiService) { }
+  tags : any = [];
+  constructor(private apiService : ApiService, private tagService : TagService) { }
+
 
   ngOnInit(): void {
-    this.getBooks(); 
+    this.getBooks();
+    this.getTags();
   }
 
   getBooks(){
     this.apiService.getBooks().subscribe((data) => {
       this.books = data;
+    });
+  }
+
+  getTags(){
+    this.tagService.getTags().subscribe((data) => {
+      this.tags = data;
     });
   }
 }
---
+++ src/app/services/tag.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TagService {
+
+  API_URL : String = 'https://bibliovotes-production.up.railway.app/api/tag';
+  constructor(private httpClient : HttpClient) { }
+
+  getTags(){
+    return this.httpClient.get(`${this.API_URL}`);
+}
+}
